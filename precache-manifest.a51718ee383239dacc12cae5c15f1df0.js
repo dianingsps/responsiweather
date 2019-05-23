d2ace24113c7b71f3bd2f7e0ed0e550240bcdea5
dianingsps <dianingsukmapilia@gmail.com>
2019-05-23 11:53:16 +0700

    Updates

---
+++ precache-manifest.a51718ee383239dacc12cae5c15f1df0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e62cb9fa395d823f8c63ec10accf2ca6",
+    "url": "/responsiweather/index.html"
+  },
+  {
+    "revision": "a8ead9ec051d3233312b",
+    "url": "/responsiweather/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/responsiweather/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a8ead9ec051d3233312b",
+    "url": "/responsiweather/static/js/main.527ae781.chunk.js"
+  },
+  {
+    "revision": "91191df5edc176f12132",
+    "url": "/responsiweather/static/js/runtime~main.cb007717.js"
+  }
+]);
